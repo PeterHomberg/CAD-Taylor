704abef7a3f6dc64a1893838101ff7e10fcf5116
Peter Homberg <peter.homberg@durr.com>
2025-07-19 15:18:18 +0800

    Initial Commit

---
+++ CAD Taylor/CAD_TaylorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAD_TaylorApp.swift
+//  CAD Taylor
+//
+//  Created by Peter Homberg on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CAD_TaylorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
